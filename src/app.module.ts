1ba1bd2206fff8f390658be53335a941e659fbc8
유경아 <kayoo@eluocnc.com>
2024-03-15 16:31:22 +0900

    add: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { EmailModule } from "./email/email.module";
 import { validationSchema } from "./config/validationSchema";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
@@ -10,6 +11,16 @@ import { validationSchema } from "./config/validationSchema";
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3336,
+      username: "root",
+      password: "test",
+      database: "test",
+      entities: [`${__dirname}/**/*.entity{.ts, .js}`],
+      synchronize: true, // 코드 기반 DB 동기화 (true => 실행마다 초기화)
     }),
     UsersModule,
     EmailModule,
